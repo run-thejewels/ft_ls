085b45d50572af46d601cfed8f1e94de39bc89e2
kbethany <apemi@mail.ru>
2019-10-08 22:40:17 +0300

    Sort added(not completed)

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -35,11 +35,61 @@ void		print_files(t_list *files_list, uint32_t flags)
 	}
 }
 
-void		sort_files(t_list *files_list, uint32_t flags)
+void        swap_nodes(t_list *lst, t_list_node *l, t_list_node *r)
 {
-	//TODO: IMPLEMENT
-	(void)files_list;
-	flags++;
+
+    if (l->prev)
+        l->prev->next = r;
+    else
+        lst->begin = r;
+    if (r->next)
+        r->next->prev = l;
+    else
+        lst->end = l;
+    l->next = r->next;
+    r->next = l;
+    r->prev = l ->prev;
+    l->prev = r;
+}
+int         time_sort(t_filedata *a, t_filedata *b)
+{
+
+}
+
+int         size_sort(t_filedata *a, t_filedata *b)
+{
+    return ((int)(b->size - a->size));
+}
+
+int         name_sort(t_filedata *a, t_filedata *b)
+{
+    return(ft_strcmp(a->name, b->name));
+}
+
+void		lst_sort(t_list *files_list,int (*f)(void *, void *), char rev)
+{
+    t_list_node *tmp;
+    t_list_node *pmt;
+
+    tmp = files_list->end;
+    while (tmp) {
+        pmt = files_list->begin;
+        while (pmt->next) {
+            if (f(pmt->content, pmt->next->content) * (rev ? -1 : 1) > 0)
+                swap_nodes(files_list, pmt, pmt->next);
+            else
+                pmt = pmt->next;
+        }
+        tmp = tmp->prev;
+    }
+}
+void        sort_files(t_list *files_list, uint32_t flags)
+{
+    lst_sort(files_list, name_sort, flags & F_REVERSE);
+//    if (flags & F_TIME)
+//        lst_sort(files_list, time_sort, flags & F_REVERSE);
+    if (flags & F_TIME)
+        lst_sort(files_list, size_sort, flags & F_REVERSE);
 }
 
 void		free_fileslst(t_list *flist)
